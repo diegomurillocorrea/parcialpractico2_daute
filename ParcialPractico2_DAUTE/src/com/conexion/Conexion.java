c7c9aa7d80d7c7d7518eb8a8f33a3027d0ee2b97
diego murillo <diegomurillocorrea@gmail.com>
2022-09-18 08:41:57 -0600

    Conexion file created

--- ParcialPractico2_DAUTE/src/com/conexion/Conexion.java
+++ ParcialPractico2_DAUTE/src/com/conexion/Conexion.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.conexion;
 
-/**
- *
- * @author diegomurillo
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
     
+    private Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public void conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/db_biblioteca",
+                    "root",
+                    ""
+            );
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+        }
+    }
+
+    public void desconectar() {
+        try {
+            if (!con.isClosed()) {
+                con.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al desconectar: " + e.getMessage());
+        }
+    }
 }
